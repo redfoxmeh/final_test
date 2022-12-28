07b8eccdc9a66d26fb59476fdd99c63c93d16dff
redfoxmeh <108362221+redfoxmeh@users.noreply.github.com>
2022-12-28 22:31:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string [] array = { "Hello", ", ", "World", "!", "I", "am", "Groot", "42" };
+ArrayChecking(array);
+void ArrayChecking(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+}
